f5c47a085d51f4bc1db831f578c58003a61d3f81
milou <milouxp@gmail.com>
2020-07-07 01:48:50 +0900

    add go samples

---
+++ README.md
@@ -0,0 +1,22 @@
+# [Golang](https://golang.org/)
+
+
+## Hello.go
+
+```bash
+go build hello.go
+```
+
+```go
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("hello, world\n")
+}
+```
+
+
+## [filepath](https://golang.org/pkg/path/filepath/)
+
---
+++ filepath/filepath_Join.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+)
+
+func main() {
+	fmt.Println("On Unix:")
+	fmt.Println(filepath.Join("a", "b", "c"))
+	fmt.Println(filepath.Join("a", "b/c"))
+	fmt.Println(filepath.Join("a/b", "c"))
+	fmt.Println(filepath.Join("a/b", "/c"))
+}
